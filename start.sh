ec17ac488d4e9255cd2f41d32bdbcc106577e527
Peko Atanasov <peko@MacBookPros-MacBook-Pro.local>
2017-12-05 13:17:12 +0200

    arg

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ version=0.0.1
 mkdir -p logs
 rm -r logs/* 2> /dev/null
 #start SocketServer to feed json data for Spark
-nohup python datafeed/datafeeder.py conf/iata.txt data/predictions/test.csv >> logs/datafeed.log &
+nohup python datafeed/datafeeder.py conf/iata.txt data/predictions/test.csv data/predictions/actual.csv >> logs/datafeed.log &
 echo $! >> pids
 #start spark streaming to aggregate the data into parquet files
 nohup $spark_submit --class com.cba.weather.spark.stream.SocketStreamingJob spark/target/spark-$version-jar-with-dependencies.jar  > logs/streaming.log &
